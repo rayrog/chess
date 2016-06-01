8ef1d3666fdf43260f118618b541324a11c0ce2b
dana23746 <luka.dujardin@somfy.com>
2016-06-01 12:11:27 +0200

    Roi créer

--- model/Main.java
+++ model/Main.java
@@ -22,11 +22,7 @@ public class Main{
 		} else {
 			System.out.println("Mouvement impossible gros con !");
 		}
-		
-		
-		
-		
-		
+				
 	}
 
 }
---
+++ model/Roi.java
@@ -0,0 +1,26 @@
+package model;
+
+import tools.ChessPiecesFactory;
+
+public class Roi extends AbstractPiece
+{
+
+	public Roi(Couleur couleur, Coord coord) {
+		super(couleur, coord);
+	}
+
+	public boolean isMoveOk(int xFinal,int yFinal,boolean isCatchOk,boolean isCastlingPossible)
+	{
+		// sait ou elle est , a ses coordonn�, et coordonn� de destination donn�
+		if ((xFinal <= (this.getX()-1) && xFinal >= (this.getX()+1))&&(yFinal <= (this.getY()-1) && yFinal >= (this.getY()+1))){
+			
+			return true;
+		}
+		else {
+			return false;		
+		}
+	}
+
+
+	
+}
